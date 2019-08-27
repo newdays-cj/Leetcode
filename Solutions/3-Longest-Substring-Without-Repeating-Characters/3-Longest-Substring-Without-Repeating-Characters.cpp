4f2fd47459a675b2aca5af6576a6eba3f24e42f9
ChenJun <newdays-cj@live.com>
2019-08-28 01:11:14 +0800

    Leetcode: Add solution for 3 Longest Substring Without Repeating Characters
    Signed-off-by: ChenJun <newdays-cj@live.com>

---
+++ Solutions/3-Longest-Substring-Without-Repeating-Characters/3-Longest-Substring-Without-Repeating-Characters.cpp
@@ -0,0 +1,52 @@
+﻿// 3-Longest-Substring-Without-Repeating-Characters.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+class Solution {
+public:
+	int lengthOfLongestSubstring(string s) {
+		int post[256];
+		for (int i = 0; i < 256; ++i) {
+			post[i] = -1;
+		}
+		int max_length = 0;
+		int per_length = 0;
+		for (int i = 0; i < s.size(); ++i) {
+			if ((i - per_length) <= post[s[i]]) {
+				per_length = i - post[s[i]];
+			}
+			else {
+				++per_length;
+				if (per_length > max_length)
+					max_length = per_length;
+			}
+			post[s[i]] = i;
+		}
+		return max_length;
+	}
+};
+
+int main()
+{
+	Solution solution;
+	string input = "aabaab!bb";
+	int ret = solution.lengthOfLongestSubstring(input);
+	std::cout << ret << endl;
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
