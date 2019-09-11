dd39b7dc96b5a9c1a86a6d22de90d2a5f8b99c74
ChenJun <newdays-cj@live.com>
2019-09-11 23:41:52 +0800

    Leetcode : Add solution for 441 Arranging Coins
    Signed-off-by: ChenJun <newdays-cj@live.com>

---
+++ Solutions/441-Arranging-Coins/441-Arranging-Coins.cpp
@@ -0,0 +1,28 @@
+﻿// 441-Arranging-Coins.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include <math.h>
+
+class Solution {
+public:
+	int arrangeCoins(int n) {
+		long __n = n;
+		if (__n <= 0) {
+			return 0;
+		}
+
+		long k = pow(2 * (double)__n, 0.5) - 1;
+		/* (k + 1) * k / 2 <= n < (k + 2) * (k + 1) / 2 */
+		while ((k + 2) * (k + 1) <= 2 * __n) {
+			k++;
+		}
+		return k;
+	}
+};
+
+int main()
+{
+	Solution solution;
+	solution.arrangeCoins(2147483647);
+}
