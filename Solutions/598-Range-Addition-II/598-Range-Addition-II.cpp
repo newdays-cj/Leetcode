1af9d78a69281e77132565d5dd3632db2735ca9c
ChenJun <newdays-cj@live.com>
2019-10-15 00:23:54 +0800

    Leetcode : Add solutions for 598 Range Addition II
    Signed-off-by: ChenJun <newdays-cj@live.com>

---
+++ Solutions/598-Range-Addition-II/598-Range-Addition-II.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+	int maxCount(int m, int n, vector<vector<int>>& ops) {
+		int min_r = m;
+		int min_c = n;
+		for (int i = 0; i < ops.size(); i++) {
+			if (ops[i][0] < min_r) {
+				min_r = ops[i][0];
+			}
+			if (ops[i][1] < min_c) {
+				min_c = ops[i][1];
+			}
+		}
+		return min_r * min_c;
+	}
+};
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
