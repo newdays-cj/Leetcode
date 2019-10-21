f5e01d6a2f934f23e5bb1c15e6300165caccb374
ChenJun <newdays-cj@live.com>
2019-10-22 00:18:55 +0800

    Leetcode: Add solution for 1232 check If It Is a Straight Line
    Signed-off-by: ChenJun <newdays-cj@live.com>

---
+++ Solutions/1232-Check-If-It-Is-a-Straight-Line/1232-Check-If-It-Is-a-Straight-Line.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+class Solution {
+public:
+	bool checkStraightLine(vector<vector<int>>& coordinates) {
+		if (coordinates.size() < 3) {
+			return true;
+		}
+		for (int i = 2; i < coordinates.size(); i++) {
+			if ((coordinates[i][0] - coordinates[i - 1][0]) * (coordinates[1][1] - coordinates[0][1]) !=
+				(coordinates[1][0] - coordinates[0][0]) * (coordinates[i][1] - coordinates[i - 1][1])) {
+				return false;
+			}
+		}
+		return true;
+	}
+};
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
