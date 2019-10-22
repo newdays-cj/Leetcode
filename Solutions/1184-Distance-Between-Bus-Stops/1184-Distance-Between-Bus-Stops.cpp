2cbcb084a9e6dac8a6e249db83e0966e5c825a28
ChenJun <newdays-cj@live.com>
2019-10-23 00:12:24 +0800

    Leetcode: Add solution for 1184 Distance Between Bus Stops
    Signed-off-by: ChenJun <newdays-cj@live.com>

---
+++ Solutions/1184-Distance-Between-Bus-Stops/1184-Distance-Between-Bus-Stops.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+	int distanceBetweenBusStops(vector<int>& distance, int start, int destination) {
+		int all = 0;
+		for (int i = 0; i < distance.size(); i++) {
+			all += distance[i];
+		}
+
+		int tmp = 0;
+		if (start > destination) {
+			destination = destination + distance.size();
+		}
+
+		for (int i = start; i < destination; i++) {
+			tmp += distance[i % distance.size()];
+		}
+
+		return tmp > all - tmp ? all - tmp : tmp;
+	}
+};
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
