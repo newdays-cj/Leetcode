2c9a88c7cd4e6758372aa74c3470fec43f871c26
ChenJun <newdays-cj@live.com>
2019-10-25 23:22:49 +0800

    Leetcode: Add solution for 1103 Distribute Candies to People
    Signed-off-by: ChenJun <newdays-cj@live.com>

---
+++ Solutions/1103-Distribute-Candies-to-People/1103-Distribute-Candies-to-People.cpp
@@ -0,0 +1,26 @@
+﻿#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+	vector<int> distributeCandies(int candies, int num_people) {
+		vector<int> ret = vector<int>(num_people, 0);
+		int total = 0;
+		int i = 0;
+		while (1) {
+			for (int j = 0; j < num_people; j++) {
+				int tmp = i * num_people + j + 1;
+				if (candies - total > tmp) {
+					ret[j] += tmp;
+					total += tmp;
+				}
+				else {
+					ret[j] += candies - total;
+					return ret;
+				}
+			}
+			i++;
+		}
+	}
+};
